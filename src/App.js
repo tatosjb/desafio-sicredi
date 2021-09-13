d6023b1c41891e08284d255c6bcc565040be4a7f
Altair J. Fernandes <tatosjb@gmail.com>
2021-09-13 17:20:06 -0300

    unused imports

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
 import PrivateRoute from './components/PrivateRoute';
 
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams,
-} from "react-router-dom"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
 
 import { SessionContextProvider } from "./contexts/SessionContext";
 
-import Header from "./components/Header"
 import Login from "./pages/Login"
 import DragonsIndex from './pages/Dragons/Index'
 
@@ -19,10 +12,6 @@ function App() {
   return (
     <SessionContextProvider>
       <Router>
-        {/* <Link to="/dragons">List Dragons</Link>
-        <Link to="/dragon/1">Look Dragon</Link>
-        <Link to="/dragon/1/edit">Edit Dragon</Link> */}
-
         <Switch>
           <Route path="/login" exact>
             <Login />
